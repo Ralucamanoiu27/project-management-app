c1836393654264e130efe2d980a70a8df32bc5df
Raluca Manoiu <raluca.manoiu@yahoo.com>
2019-12-08 10:37:40 +0200

    initial commit

---
+++ src/test/java/com/sda10/finalproject/projectmanagement/ProjectmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda10.finalproject.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
