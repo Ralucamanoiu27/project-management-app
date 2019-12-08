991f7ca5f21610423c5a15ebc3e00a107c642051
cristinam <mateescu.cristina24@yahoo.ro>
2019-12-08 11:55:48 +0200

    controller

--- src/main/java/com/sda10/finalproject/projectmanagement/controller/ProjectsController.java
+++ src/main/java/com/sda10/finalproject/projectmanagement/controller/ProjectsController.java
@@ -1,4 +1,16 @@
 package com.sda10.finalproject.projectmanagement.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.annotation.PostConstruct;
+
+@RestController
 public class ProjectsController {
+
+    @PostMapping("/projects")
+    public String createProject(){
+        return "MyProject";
+    }
 }
--- src/test/java/com/sda10/finalproject/projectmanagement/controller/ProjectsControllerRestIntegrationTest.java
+++ src/test/java/com/sda10/finalproject/projectmanagement/controller/ProjectsControllerRestIntegrationTest.java
@@ -1,4 +1,24 @@
 package com.sda10.finalproject.projectmanagement.controller;
 
-public class ProjectsControllerRestIntegrationTest {
+import com.sda10.finalproject.projectmanagement.RestIntegrationTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+public class ProjectsControllerRestIntegrationTest extends RestIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void WhenAPostRequestIsReceived_thenAprojectIsCreated() {
+            String RelativePath="/projects";
+            ResponseEntity<String> response = this.restTemplate
+                    .postForEntity(url(RelativePath), null, String.class);
+            Assertions.assertEquals("MyProject",response.getBody());
+    }
+
+
 }
